a508246aca88729681629dd972faccb043ef56ae
Alex Thomas <alexthomas93@users.noreply.github.com>
2024-12-03 12:01:03 +0000

    Replaced deprecated langchain_community imports with langchain_neo4j imports

--- 2-llm-rag-python-langchain/cypher_chain.py
+++ 2-llm-rag-python-langchain/cypher_chain.py
@@ -3,11 +3,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from langchain_openai import ChatOpenAI
-from langchain_community.graphs import Neo4jGraph
-from langchain.chains import GraphCypherQAChain
+from langchain_neo4j import GraphCypherQAChain, Neo4jGraph
 from langchain.prompts import PromptTemplate
 
-llm = ChatOpenAI(openai_api_key=os.getenv('OPENAI_API_KEY'))
+llm = ChatOpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"), temperature=0.0, seed=1)
 
 graph = Neo4jGraph(
     url=os.getenv('NEO4J_URI'),
@@ -32,7 +31,8 @@ cypher_chain = GraphCypherQAChain.from_llm(
     llm,
     graph=graph,
     cypher_prompt=cypher_generation_prompt,
-    verbose=True
+    verbose=True,
+    allow_dangerous_requests=True,
 )
 
 cypher_chain.invoke({"query": "What is the plot of the movie Toy Story?"})
